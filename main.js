3b2b7b1be55adf99d271a5d601629695044d2f38
s100tist <s100tist@pop-os.localdomain>
2023-04-18 22:03:23 -0600

    js agregado 2

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ const pixel = canvas.getContext('2d')
 
 //probando canvas
 pixel.fillRect(0,0, canvas.width,canvas.height)
+
+const pelota => ({
+    x:canvas.width/2,
+    y:canvas.height/2,
+    color,
+    draw(){
+        //ctx.fillStyle
+    }
+})
