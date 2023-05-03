8a74c1e65d1ac0b1177fac14ed10e30404844503
corvo <corvohyatt@gmail.com>
2023-05-03 12:01:10 -0600

    Avance

--- main.js
+++ main.js
@@ -6,11 +6,32 @@ changeBoton=() => {
 //Inicialización del campo
 Crafty.init(1366, 900, document.getElementById('game'))
 
-//Texto y Botones
 // Ubicación del Timer
+let time=0
+let Tiempo=Crafty.e('2D,DOM,Text').attr({
+    x: 200,
+    y: 200,
+    w: 265,
+    h: 50
+});
 
+Tiempo.textFont({
+    size: '20px',
+}).textColor('white');
 //Tiempo en formato mm:ss
-
+// Tiempo.bind('UpdateFrame', (eventData) => {
+//     time+=eventData.dt/1000;
+//     let minutos=parseInt((time/60), 10);
+//     if (minutos<10) minutos='0'+minutos;
+//     let segundos=parseInt((time%60), 10);
+//     if (segundos<10) segundos='0'+segundos;
+//     if (minutos==10) {
+//         Crafty.stop(); go1=0; go2=0; time=0;
+//         this.text("Se acabo el partido!");
+//     } else {
+//         this.text("Tiempo: "+minutos+":"+segundos);
+//     }
+// });
 // Ubicación del Marcador
 
 // Formato Marcador de goles
@@ -156,3 +177,89 @@ var initPosition=() => {
     team_2[6].attr({ x: 420, y: 400, w: 85, h: 85 })
 
 }
+// Espacio de desplazamiento establedo para cada jugador
+for (let i=0; i<7; i++) {
+    // Espacio jugador equipo1
+    team_1[i].reel("walking", 500, [
+        [0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0],
+        [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1],
+    ]);
+    team_1[i].animate("walking", -1);
+
+    //Espacio jugador equipo2
+    team_2[i].reel("walking", 500, [
+        [7, 1], [6, 1], [5, 1], [4, 1], [3, 1], [2, 1], [1, 1], [0, 1],
+        [7, 0], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]
+    ]);
+    team_2[i].animate("walking", -1);
+}
+//Movimiento del balón
+let xball;
+let yball;
+ball.origin("center");
+ball.bind("UpdateFrame", (eventData) => {
+    if (this.x>950||!this.x_move) {
+        xball=true;
+        this.x_move=false;
+    }
+    if (this.x<55||this.x_move) {
+        xball=false;
+        this.x_move=true;
+    }
+    if (xball) {
+        this.x=this.x-222*(eventData.dt/1000);
+        this.rotation=this.rotation-8;
+    }
+    else {
+        this.x=this.x+222*(eventData.dt/1000);
+        this.rotation=this.rotation+8;
+    }
+    if (this.y>=595||this.y_move) {
+        yball=true;
+        this.y_move=true;
+    }
+    if (this.y<=17||!this.y_move) {
+        yball=false;
+        this.y_move=false;
+    }
+    let velocidad=200;
+    if (this.y_tiro) {
+        velocidad=22
+    }
+    if (yball) {
+        this.y=this.y-velocidad*(eventData.dt/1000);
+    }
+    else {
+        this.y=this.y+velocidad*(eventData.dt/1000);
+    }
+});
+
+//Movimiento de los jugadores
+
+let y1;
+let x1;
+team_1[0].bind("UpdateFrame", (eventData) => {
+    if (this.x>68) {
+        x1=true;
+    }
+    if (this.x<28) {
+        x1=false;
+    }
+    if (x1)
+        this.x=this.x-30*(eventData.dt/1000);
+    else
+        this.x=this.x+30*(eventData.dt/1000);
+
+    if (this.y>=350) {
+        y1=true;
+    }
+    if (this.y<=190) {
+        y1=false;
+    }
+    if (y1) {
+        this.y=this.y-68*(eventData.dt/1000);
+    }
+    else {
+        this.y=this.y+73*(eventData.dt/1000);
+    }
+});
