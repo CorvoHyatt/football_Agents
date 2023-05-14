4f7496f696591439ce4e2c68e79638b4aee7f58c
corvohyatt <corvohyatt@gmail.com>
2023-05-14 07:50:21 -0600

    movimiento balon y marcadores listos

--- main.js
+++ main.js
@@ -1,37 +1,87 @@
-changeBoton=() => {
-    if (document.getElementById('pausa').innerHTML=='Pause') {
-        document.getElementById('pausa').innerHTML='Resume0';
-    } else { document.getElementById('pausa').innerHTML='Pause'; }
+changeBoton = () => {
+    if (document.getElementById('pausa').innerHTML == 'Pausa') {
+        document.getElementById('pausa').innerHTML = 'Continuar';
+    } else { document.getElementById('pausa').innerHTML = 'Pausa'; }
 }
 //Inicialización del campo
-Crafty.init(1366, 900, document.getElementById('game'))
+Crafty.init(1366, 900, document.getElementById('game'));
+//!Timer
+// Obtén una referencia al elemento de texto del contador de tiempo
+var timer = Crafty("h1#timer");
+var tiempoPausado = false;
+// Variables para el contador de tiempo
+var tiempo = 0;
+var intervalo;
 
-// Ubicación del Timer
-let time=0
-let Tiempo=Crafty.e('2D,DOM,Text').attr({
-    x: 200,
-    y: 200,
-    w: 265,
-    h: 50
-});
+// Función para formatear el tiempo en formato HH:MM:SS
+function formatearTiempo(tiempo) {
+    var horas = Math.floor(tiempo / 3600);
+    var minutos = Math.floor((tiempo % 3600) / 60);
+    var segundos = tiempo % 60;
 
-Tiempo.textFont({
-    size: '20px',
-}).textColor('white');
-//Tiempo en formato mm:ss
-// Tiempo.bind('UpdateFrame', (eventData) => {
-//     time+=eventData.dt/1000;
-//     let minutos=parseInt((time/60), 10);
-//     if (minutos<10) minutos='0'+minutos;
-//     let segundos=parseInt((time%60), 10);
-//     if (segundos<10) segundos='0'+segundos;
-//     if (minutos==10) {
-//         Crafty.stop(); go1=0; go2=0; time=0;
-//         this.text("Se acabo el partido!");
-//     } else {
-//         this.text("Tiempo: "+minutos+":"+segundos);
-//     }
-// });
+    // Asegurarse de tener dos dígitos para cada componente de tiempo
+    var tiempoFormateado =
+        ("0" + horas).slice(-2) +
+        ":" +
+        ("0" + minutos).slice(-2) +
+        ":" +
+        ("0" + segundos).slice(-2);
+
+    return tiempoFormateado;
+}
+
+// Función para iniciar el contador de tiempo
+function iniciarTiempo() {
+    intervalo = setInterval(function () {
+        tiempo++;
+        timer.text(formatearTiempo(tiempo));
+    }, 1000); // Actualizar cada segundo (1000 ms)
+}
+function pausarTiempo() {
+    clearInterval(intervalo);
+    tiempoPausado = true;
+}
+function reanudarTiempo() {
+    if (tiempoPausado) {
+        intervalo = setInterval(function () {
+            tiempo++;
+            timer.text(formatearTiempo(tiempo));
+        }, 1000); // Actualizar cada segundo (1000 ms)
+        tiempoPausado = false;
+    }
+}
+// Función para detener el contador de tiempo
+function detenerTiempo() {
+    clearInterval(intervalo);
+}
+
+// Evento click para el botón "Iniciar Juego"
+Crafty("button#iniciar-juego").bind("click", function () {
+    initPosition();
+    Crafty.init();
+    ball.attr({ x: (1366 / 2) - 20, y: 430, w: 40, h: 40 });
+    iniciarTiempo();
+  });
+  
+  Crafty("button#parar-juego").bind("click", function () {
+    time = 0;
+    initPosition();
+    Crafty.stop();
+    goles1 = 0;
+    goles2 = 0;
+    marcador.text('Equipo1: ' + goles1 + '  Equipo2: ' + goles2);
+    detenerTiempo();
+  });
+  
+  Crafty("button#pausa").bind("click", function () {
+    if (!tiempoPausado) {
+      pausarTiempo();
+    } else {
+      reanudarTiempo();
+    }
+  });
+  
+
 // Ubicación del Marcador
 
 // Formato Marcador de goles
@@ -40,7 +90,7 @@ Tiempo.textFont({
 //Inicialización de los jugadores
 
 // Equipo_1
-let assetsObj={
+let assetsObj = {
     "sprites": {
         "equipo_1.png": {
             tile: 104,
@@ -56,7 +106,7 @@ let assetsObj={
 Crafty.load(assetsObj);
 
 //Equipo 2
-let assetsObj2={
+let assetsObj2 = {
     "sprites": {
         "equipo_2.png": {
             tile: 104,
@@ -72,7 +122,7 @@ let assetsObj2={
 Crafty.load(assetsObj2);
 
 //Inicialización del balon
-let assetsObjBall={
+let assetsObjBall = {
     "sprites": {
         "balon.png": {
             tile: 306,
@@ -88,44 +138,31 @@ let assetsObjBall={
 Crafty.load(assetsObjBall);
 
 //! Cración de las Porterias
-// let red1 = Crafty
-//                 .e("2D, Canvas, Color, Collision")
-//                 .attr({x:28, y: 280, w:28, h82})
-//                 .color('green')
-//                 .collision()
-//                 .checkHits("ball_start")
-//                 .bind("HitOn", function(hitData) {
-//                     if((hitDatas = this.hit('ball_start'))){
-//                         go2 += 1;
-//                         Crafty.stop();
-//                         initPosition();
-//                         //Logica para cuando se mete un gol
-//                     }
-//                 });
+
 //! Cración del Balon
-let ball=Crafty
+let ball = Crafty
     .e('2D, Canvas, ball_start,SpriteAnimation, Collision')
-    .attr({ x: (1366/2)-20, y: 430, w: 40, h: 40 })
+    .attr({ x: (1366 / 2) - 20, y: 430, w: 40, h: 40 })
     .collision()
     .checkHits('Collision')
-    .bind("HitOn", (hitData) => {
+    .bind("HitOn", function (hitData) {
         //desplazamientos
-        if ((hitData=this.hit('arriba'))) {
-            this.y_move=false;
-            this.y_tiro=false;
+        if ((hitData = this.hit('arriba'))) {
+            this.y_move = false;
+            this.y_tiro = false;
         }
-        if ((hitData=this.hit('abajo'))) {
-            this.y_move=true;
-            this.y_tiro=false;
+        if ((hitData = this.hit('abajo'))) {
+            this.y_move = true;
+            this.y_tiro = false;
         }
-        if ((hitData=this.hit('centro'))) {
-            this.y_tiro=true;
+        if ((hitData = this.hit('centro'))) {
+            this.y_tiro = true;
         }
-        if ((hitData=this.hit('walker_start'))) {
-            this.x_move=true;
+        if ((hitData = this.hit('walker_start'))) {
+            this.x_move = true;
         }
-        if ((hitData=this.hit('walker2_start'))) {
-            this.x_move=false;
+        if ((hitData = this.hit('walker2_start'))) {
+            this.x_move = false;
         }
     });
 ball.reel("walking", 500, [
@@ -134,8 +171,8 @@ ball.reel("walking", 500, [
 ball.animate("walking", -1);
 
 //! Creación de los jugadores
-let team_1=[]
-let team_2=[]
+let team_1 = []
+let team_2 = []
 
 // Equipo_1
 team_1.push(Crafty.e("2D, Canvas, walker_start, SpriteAnimation, Collision, centro").attr({ x: 30, y: 400, w: 85, h: 85 }).collision())
@@ -158,7 +195,7 @@ team_2.push(Crafty.e("2D, Canvas, walker2_start, SpriteAnimation, Collision, cen
 
 //Posicionamiento de los jugadores
 //Equipo 1
-var initPosition=() => {
+ function initPosition () {
     team_1[0].attr({ x: 30, y: 400, w: 85, h: 85 })
     team_1[1].attr({ x: 300, y: 300, w: 85, h: 85 })
     team_1[2].attr({ x: 300, y: 500, w: 85, h: 85 })
@@ -178,7 +215,7 @@ var initPosition=() => {
 
 }
 // Espacio de desplazamiento establedo para cada jugador
-for (let i=0; i<7; i++) {
+for (let i = 0; i < 7; i++) {
     // Espacio jugador equipo1
     team_1[i].reel("walking", 500, [
         [0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0],
@@ -197,40 +234,40 @@ for (let i=0; i<7; i++) {
 let xball;
 let yball;
 ball.origin("center");
-ball.bind("UpdateFrame", (eventData) => {
-    if (this.x>950||!this.x_move) {
-        xball=true;
-        this.x_move=false;
+ball.bind("UpdateFrame",function(eventData) {
+    if (this.x > 950 || !this.x_move) {
+        xball = true;
+        this.x_move = false;
     }
-    if (this.x<55||this.x_move) {
-        xball=false;
-        this.x_move=true;
+    if (this.x < 55 || this.x_move) {
+        xball = false;
+        this.x_move = true;
     }
     if (xball) {
-        this.x=this.x-222*(eventData.dt/1000);
-        this.rotation=this.rotation-8;
+        this.x = this.x - 222 * (eventData.dt / 1000);
+        this.rotation = this.rotation - 8;
     }
     else {
-        this.x=this.x+222*(eventData.dt/1000);
-        this.rotation=this.rotation+8;
+        this.x = this.x + 222 * (eventData.dt / 1000);
+        this.rotation = this.rotation + 8;
     }
-    if (this.y>=595||this.y_move) {
-        yball=true;
-        this.y_move=true;
+    if (this.y >= 595 || this.y_move) {
+        yball = true;
+        this.y_move = true;
     }
-    if (this.y<=17||!this.y_move) {
-        yball=false;
-        this.y_move=false;
+    if (this.y <= 17 || !this.y_move) {
+        yball = false;
+        this.y_move = false;
     }
-    let velocidad=200;
+    let velocidad = 200;
     if (this.y_tiro) {
-        velocidad=22
+        velocidad = 22
     }
     if (yball) {
-        this.y=this.y-velocidad*(eventData.dt/1000);
+        this.y = this.y - velocidad * (eventData.dt / 1000);
     }
     else {
-        this.y=this.y+velocidad*(eventData.dt/1000);
+        this.y = this.y + velocidad * (eventData.dt / 1000);
     }
 });
 
@@ -239,27 +276,27 @@ ball.bind("UpdateFrame", (eventData) => {
 let y1;
 let x1;
 team_1[0].bind("UpdateFrame", (eventData) => {
-    if (this.x>68) {
-        x1=true;
+    if (this.x > 68) {
+        x1 = true;
     }
-    if (this.x<28) {
-        x1=false;
+    if (this.x < 28) {
+        x1 = false;
     }
     if (x1)
-        this.x=this.x-30*(eventData.dt/1000);
+        this.x = this.x - 30 * (eventData.dt / 1000);
     else
-        this.x=this.x+30*(eventData.dt/1000);
+        this.x = this.x + 30 * (eventData.dt / 1000);
 
-    if (this.y>=350) {
-        y1=true;
+    if (this.y >= 350) {
+        y1 = true;
     }
-    if (this.y<=190) {
-        y1=false;
+    if (this.y <= 190) {
+        y1 = false;
     }
     if (y1) {
-        this.y=this.y-68*(eventData.dt/1000);
+        this.y = this.y - 68 * (eventData.dt / 1000);
     }
     else {
-        this.y=this.y+73*(eventData.dt/1000);
+        this.y = this.y + 73 * (eventData.dt / 1000);
     }
 });
